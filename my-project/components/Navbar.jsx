ca8941c9f85b55dfc533083e03b107b94af8c5a5
tanmayiepatil <120322328+Tanmayie03@users.noreply.github.com>
2024-05-20 00:20:39 +0530

    feat: Started the projecy

---
+++ my-project/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div>
+      <div className="bg-white shadow-b-2 px-12 shadow-slate-100 shadow-lg">
+        <img src="../src/assets/images/airbnbLogo.png" className="w-36 " />
+      </div>
+    </div>
+  );
+}
--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,8 +1,13 @@
+import Navbar from "../components/Navbar";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-center italic underline">
-      Hello airbnb!
-    </h1>
+    <>
+      <Navbar />
+      <h1 className="text-3xl font-bold text-center italic underline">
+        Hello airbnb!
+      </h1>
+    </>
   );
 }
 
