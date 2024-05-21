6129d707ec30d1739b1c27d2ef14879180d998e1
tanmayiepatil <120322328+Tanmayie03@users.noreply.github.com>
2024-05-21 22:59:58 +0530

    feat: Added some content

---
+++ my-project/components/FirstSection.jsx
@@ -0,0 +1,21 @@
+const FirstSection = () => {
+  return (
+    <>
+      <div className="">
+        <img
+          src="../src/assets/images/photoGrid.png"
+          className="w-3/4 mx-auto my-16 "
+        />
+        <div className="px-16">
+          <h1 className="py-6 text-4xl font-bold">Online Experiences</h1>
+          <h1 className="py-2 text-base font-light">
+            Join unique interactive activities led by one-of-a-kind hosts—all
+            without leaving home.
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FirstSection;
--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,12 +1,11 @@
+import FirstSection from "../components/FirstSection";
 import Navbar from "../components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1 className="text-3xl font-bold text-center italic underline">
-        Hello airbnb!
-      </h1>
+      <FirstSection />
     </>
   );
 }
