c66d215b1cb62da0fb3fa7ccd45eaea65dfb13f6
tanmayiepatil <120322328+Tanmayie03@users.noreply.github.com>
2024-06-04 15:58:01 +0530

    feat:Added allgames component

--- my-project/components/AllGames.jsx
+++ my-project/components/AllGames.jsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+import Card from "./Card";
+import axios from "axios";
+
 const AllGames = () => {
-  return <div className="w-full">AllGames</div>;
+  const [gameGiveaway, setgameGiveaway] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://mocki.io/v1/b4d2e015-85f9-4513-b134-de6e4498026b")
+      .then((response) => {
+        console.log(response);
+        setgameGiveaway(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setError(error.message);
+      });
+  }, []);
+  return (
+    <div className=" h-[90.2%] overflow-hidden">
+      <div className="grid  h-[90.2%] grid-cols-3 gap-6 m-6 overflow-y-scroll">
+        {gameGiveaway.map((data) => (
+          <Card
+            key={data.id}
+            id={data.id}
+            title={data.title}
+            worth={data.worth}
+            thumbnail={data.thumbnail}
+            description={data.description}
+            users={data.users}
+            type={data.type}
+            open_giveaway={data.open_giveaway}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllGames;
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "../my-project/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "../my-project/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
